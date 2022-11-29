07160d02fb9d426e513a460d732c72343304ffd3
Travis Deshotels <travis.deshotels@gmail.com>
2022-11-28 18:05:23 -0600

    Enhancements

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def pad_right(text, number_of_spaces):
     return text + " " * (number_of_spaces - len(text))
 
 
-def get_formatted_time(unformatted_time):
-    return unformatted_time.strftime("%y %b %d %H:%M:%S")
+def get_formatted_time(unformatted_time, robust=False):
+    if robust:
+        return unformatted_time.strftime("%B %d, %Y %H:%M:%S")
+    else:
+        return unformatted_time.strftime("%y %b %d %H:%M:%S")
 
 
 def print_messages(print_all=False):
@@ -20,8 +23,11 @@ def print_messages(print_all=False):
 
 def print_message(message_id):
     msg = dao.get_message(message_id)
-    print(textwrap.fill(msg[0][0], 80))
-    print(f"{msg[0][1]} {get_formatted_time(msg[0][2])}")
+    if len(msg) == 0:
+        print("No message matched the message id")
+    else:
+        print(textwrap.fill(msg[0][0], 80))
+        print(f"{msg[0][1]} {get_formatted_time(msg[0][2], robust=True)}")
 
 
 def is_poster_configured():
@@ -48,7 +54,7 @@ def get_poster():
 
 
 def quit_was_selected(command):
-    return command == "quit"
+    return command == "quit" or command == "exit"
 
 
 def process_command(command, poster):
@@ -58,6 +64,8 @@ def process_command(command, poster):
         print_messages(print_all=True)
     elif command[0] == "post":
         dao.post_message(command[1], poster)
+    else:
+        print("Invalid command")
 
 
 def main():
